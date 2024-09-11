c47824649c4d3540a30948502cc8959281e25619
anantdibakar <divakar721304@gmail.com>
2024-09-11 20:14:46 +0200

    sale inventory details end point check in.

---
+++ src/main/java/com/ims/dto/InventoryData.java
@@ -0,0 +1,23 @@
+package com.ims.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InventoryData {
+    private LocalDateTime dateOfSale;
+    private Integer quantity;
+    private Double price;
+}
