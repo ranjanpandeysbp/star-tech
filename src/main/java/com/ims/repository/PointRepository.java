68ee99c4e0789e5fb0731efb6ce4bc338fc91be6
maheskul <mahesh.kulkarni.360@gmail.com>
2024-10-25 22:37:26 +0200

    answer implementation

---
+++ src/main/java/com/ims/repository/PointRepository.java
@@ -0,0 +1,12 @@
+package com.ims.repository;
+
+import com.ims.entity.startech.AnswerEntity;
+import com.ims.entity.startech.PointsAssignedEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PointRepository extends JpaRepository<PointsAssignedEntity, Long> {
+
+    List<PointsAssignedEntity> findPointByContrubutorId(Long contrubutorId);
+
+}
