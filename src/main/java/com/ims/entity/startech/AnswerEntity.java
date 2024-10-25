5e1f25199d597a48352c989a6f787e7fd65c9aa6
Ranjan <ranjanpandey2006@gmail.com>
2024-10-25 20:53:43 +0200

    updated code

--- src/main/java/com/ims/entity/startech/AnswerEntity.java
+++ src/main/java/com/ims/entity/startech/AnswerEntity.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -29,8 +30,8 @@ public class AnswerEntity {
     @ManyToOne
     private RequestEntity request;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime pointsAssignedAt;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private LocalDate pointsAssignedAt;
     private Integer points;
 }
